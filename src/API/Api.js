429b7f6717221d3ecb5d4d39517d3cbb40b49d42
amisha-motwani <amishamotwani143@gmail.com>
2024-10-05 18:54:49 +0530

    06:54pm changes

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:5000";
+// const BASE_URL = "http://localhost:5000";
 
-// const BASE_URL = "https://soniya-admin-pannel-backend.onrender.com";
+const BASE_URL = "https://soniya-admin-pannel-backend.onrender.com";
 
 export default BASE_URL;
