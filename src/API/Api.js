a5cf4fca09c70ee1f3b445a731da4d51c4f2a2c0
amisha-motwani <amishamotwani143@gmail.com>
2024-11-08 19:06:18 +0530

    change

--- src/API/Api.js
+++ src/API/Api.js
@@ -3,5 +3,4 @@
 // const BASE_URL = "https://soniya-admin-pannel-backend.onrender.com";
 const BASE_URL = "soniya-admin-pannel-backend-rliwyk6fr-amishamotwanis-projects.vercel.app";
 
-
 export default BASE_URL;
