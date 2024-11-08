625cae5ca3a889adbc15a907f094fd8427ec07ed
amisha-motwani <amishamotwani143@gmail.com>
2024-11-08 18:40:28 +0530

    vercel link

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,5 +1,7 @@
 // const BASE_URL = "http://localhost:5000";
 
-const BASE_URL = "https://soniya-admin-pannel-backend.onrender.com";
+// const BASE_URL = "https://soniya-admin-pannel-backend.onrender.com";
+const BASE_URL = "soniya-admin-pannel-backend-rliwyk6fr-amishamotwanis-projects.vercel.app";
+
 
 export default BASE_URL;
